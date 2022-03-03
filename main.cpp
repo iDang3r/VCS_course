0cf061c6f5742c9ff8e25aa71062d17802541e4e
Alex Rusin <rusin.ai@phystech.edu>
2022-03-03 16:03:59 +0300

    update version

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main() {
 
-    std::cout << "Hi, VCS! New release" << std::endl;
+    std::cout << "Hi, VCS! New release 1.0" << std::endl;
 
     if (rand() % 2, false) {
         std::cout << "small bug" << std::endl;
